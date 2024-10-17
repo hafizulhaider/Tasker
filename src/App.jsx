a6a33304724a7f48574eea12634f109e3ce54735
devhaider <rthafizulhaider@gmail.com>
2024-10-17 18:56:13 +0600

    Project Initialized and Design Done

---
+++ src/App.jsx
@@ -0,0 +1,23 @@
+import Footer from "./Components/Footer"
+import Header from "./Components/Header"
+import Hero from "./Components/Hero"
+import TaskBoard from "./Components/TaskBoard"
+
+
+
+function App() {
+
+
+  return (
+    <>
+     <Header/>
+     <Hero></Hero>
+     
+     <TaskBoard></TaskBoard>
+
+     <Footer></Footer>
+    </>
+  )
+}
+
+export default App
